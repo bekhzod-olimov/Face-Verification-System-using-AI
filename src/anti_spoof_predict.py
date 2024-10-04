e00845136ea2ada248c4dd2730b69b585e61eb1d
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2024-10-04 11:21:14 +0900

    Update anti_spoof_predict.py

--- src/anti_spoof_predict.py
+++ src/anti_spoof_predict.py
@@ -18,9 +18,7 @@ class Detection:
         height, width = img.shape[0], img.shape[1]
         aspect_ratio = width / height
         if img.shape[1] * img.shape[0] >= 192 * 192:
-            img = cv2.resize(img,
-                             (int(192 * math.sqrt(aspect_ratio)),
-                              int(192 / math.sqrt(aspect_ratio))), interpolation=cv2.INTER_LINEAR)
+            img = cv2.resize(img, (int(192 * math.sqrt(aspect_ratio)), int(192 / math.sqrt(aspect_ratio))), interpolation=cv2.INTER_LINEAR)
 
         blob = cv2.dnn.blobFromImage(img, 1, mean=(104, 117, 123))
         self.detector.setInput(blob, 'data')
@@ -28,9 +26,8 @@ class Detection:
         max_conf_index = np.argmax(out[:, 2])
         left, top, right, bottom = out[max_conf_index, 3]*width, out[max_conf_index, 4]*height, \
                                    out[max_conf_index, 5]*width, out[max_conf_index, 6]*height
-        bbox = [int(left), int(top), int(right-left+1), int(bottom-top+1)]
-        return bbox
-
+        
+      return [int(left), int(top), int(right - left + 1), int(bottom - top + 1)]
 
 class AntiSpoofPredict(Detection):
     def __init__(self, device_id):
